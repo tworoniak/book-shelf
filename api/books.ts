1b985cf39009c03ccb2f68237695808cab74c6f4
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-23 21:54:00 -0600

    fix vercel issue

--- api/books.ts
+++ api/books.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { toBook, type GoogleVolumeItem } from './normalize'; // (see note below)
+import { toBook, type GoogleVolumeItem } from './normalize.js';
 
 const GOOGLE_BOOKS_API = 'https://www.googleapis.com/books/v1/volumes';
 
